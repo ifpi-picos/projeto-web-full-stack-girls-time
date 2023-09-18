2e802ecef60ad245e9be93309fbb3f59fc513b5c
ly1359 <diellylopes330@gmail.com>
2023-09-18 10:53:23 -0300

    add - do projeto com configuração de dependecias necessarias

---
+++ BookBee/src/main/java/com/teamg/BookBee/BookBeeApplication.java
@@ -0,0 +1,13 @@
+package com.teamg.BookBee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookBeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookBeeApplication.class, args);
+	}
+
+}
---
+++ BookBee/src/test/java/com/teamg/BookBee/BookBeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamg.BookBee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookBeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
