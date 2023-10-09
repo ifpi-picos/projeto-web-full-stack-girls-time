c4d007b862270023f9ea2b2017a8f9f19f04a92e
ly1359 <diellylopes330@gmail.com>
2023-10-09 15:07:52 -0300

    add de comentarios Javadoc as classes

--- BookBee/src/main/java/com/teamg/BookBee/BookBeeApplication.java
+++ BookBee/src/main/java/com/teamg/BookBee/BookBeeApplication.java
@@ -3,9 +3,17 @@ package com.teamg.BookBee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+ * A Classe princiapl da aplicação BookBee.
+ * Ela usa a anotação @SpingBootApplication para iniciar a aplicação Spring Boot
+ */
 @SpringBootApplication
 public class BookBeeApplication {
 
+	/**
+	 * O método princiapl que inicia a aplicação Spring Boot.
+	 * @param args argumentos da linha de comando
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(BookBeeApplication.class, args);
 	}
--- BookBee/src/main/java/com/teamg/BookBee/service/AuthorizeService.java
+++ BookBee/src/main/java/com/teamg/BookBee/service/AuthorizeService.java
@@ -8,12 +8,27 @@ import org.springframework.stereotype.Service;
 
 import com.teamg.BookBee.repositorios.LeitorRepositorio;
 
+/**
+ * Serviço para autorização de usuarios.
+ * Esta classe implementa a interface UserDetailsService do Spring Security para fornecer
+ * detalhes do usuário a partir de um nome de usuário(neste caso, email).
+ */
 @Service
 public class AuthorizeService implements UserDetailsService{
     
+    /**
+     * Repositório para acesso aos dados dos leitores.
+     */
     @Autowired
     private LeitorRepositorio leitorRepositorio;
 
+    /** 
+     * Carrega os detalhes do usuário pelo email.
+     * 
+     * @param email o email do usuário
+     * @return os detalhes do usuário
+     * @throws UsernameNotFoundException se o usuário não for encontrado
+    */
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         return leitorRepositorio.findByEmail(email);
