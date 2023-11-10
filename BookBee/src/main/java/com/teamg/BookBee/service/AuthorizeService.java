66d04fda14748af4fbf551e232547f8642b2ea2b
ly1359 <diellylopes330@gmail.com>
2023-11-10 11:34:43 -0300

    add de log

--- BookBee/src/main/java/com/teamg/BookBee/service/AuthorizeService.java
+++ BookBee/src/main/java/com/teamg/BookBee/service/AuthorizeService.java
@@ -1,5 +1,7 @@
 package com.teamg.BookBee.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,13 +17,17 @@ public class AuthorizeService implements UserDetailsService{
     @Autowired
     private LeitorGerenciador leitorGerenciador;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthorizeService.class);
+
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
        Leitor leitor;
         try {
+            LOGGER.info("Carregando usuario pelo email: {}", email);
             leitor = leitorGerenciador.findLeitorByEmail(email);
             return leitor;
         } catch (Exception e) {
+            LOGGER.error("Erro ao carregar usuario pelo email: {}", email, e);
             throw new UsernameNotFoundException(email + "Nao encontrado");
         }
     }
--- BookBee/src/main/java/com/teamg/BookBee/service/CookieService.java
+++ BookBee/src/main/java/com/teamg/BookBee/service/CookieService.java
@@ -3,6 +3,8 @@ package com.teamg.BookBee.service;
 import java.util.Arrays;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import jakarta.servlet.http.Cookie;
@@ -12,13 +14,17 @@ import jakarta.servlet.http.HttpServletResponse;
 @Service
 public class CookieService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(CookieService.class);
+
     public static void setCookie(HttpServletResponse response, String key, String valor) {
+        LOGGER.info("Configurando cookie para a chave: {}", key);
         Cookie cookie = new Cookie(key, valor);
         cookie.setHttpOnly(true);
         response.addCookie(cookie);
     }
 
     public static String getCookie(HttpServletRequest request, String key) {
+        LOGGER.info("Obtendo cookie para a chave: {}", key);
         return Optional.ofNullable(request.getCookies())
         .flatMap(cookies -> Arrays.stream(cookies)
         .filter(cookie -> key.equals(cookie.getName()))
@@ -27,6 +33,7 @@ public class CookieService {
     }
 
     public static void deleteCookie(HttpServletResponse response, String nome) {
+        LOGGER.info("Deletando cookie para o nome: {}", nome);
         Cookie cookie = new Cookie(nome, null);
         cookie.setMaxAge(0);
         cookie.setPath("/");
